6ae4c9bf1c67d68e780376bf37db8a69d45f2244
Bruno Fenzl <brunofenzl@gmail.com>
2020-02-02 17:44:15 +0100

    Added component list to modal

--- client/src/app/content/store/reducers/page.reducer.ts
+++ client/src/app/content/store/reducers/page.reducer.ts
@@ -24,6 +24,18 @@ export function pageReducer(
         ...state,
         components: { ...state.components, [state.activeComponent]: action.payload }
       };
+    case PageActionTypes.CHOOSE_COMPONENT:
+      // Select the active component from the components list
+      const active = state.components[state.activeComponent];
+      // Add the newly created component id to it's children list
+      active.children.push(action.payload['@id']);
+      // Ensure the active component's id is set in the newly created component
+      action.payload.parent = active['@id'];
+      console.log('action.payload', action.payload);
+      return {
+        ...state,
+        components: { ...state.components, [action.payload['@id']]: action.payload }
+      };
     case PageActionTypes.ADD_COMPONENT:
       return {
         ...state,
@@ -32,11 +44,9 @@ export function pageReducer(
     case PageActionTypes.DELETE_COMPONENT:
       // First we have to find the object being deleted, here named 'theOne'.
       // 'components' is an object with all components EXCEPT the one we will remove.
-
       // The idea is to remove 'theOne' from the components object and stitch the
       // object together linking it's children to it's parent.
       const { [action.payload]: theOne, ...components } = state.components;
-      console.log('delete payload', action.payload, theOne);
       // Now, we have to get references to it's parent...
       const onesParent = components[theOne.parent];
       // ...and children
