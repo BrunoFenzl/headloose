2a0b8966940a22aeb44dda61921e79af78afc846
Bruno Fenzl <brunofenzl@gmail.com>
2020-02-05 09:57:58 +0100

    renamed project o headloose

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('kopflos app is running!');
+    expect(page.getTitleText()).toEqual('headloose app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kopflos'`, () => {
+  it(`should have as title 'headloose'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('kopflos');
+    expect(app.title).toEqual('headloose');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('kopflos app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('headloose app is running!');
   });
 });
