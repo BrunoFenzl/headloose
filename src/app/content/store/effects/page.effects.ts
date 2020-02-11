1b6e7ed9c2be51af0c6a5fdc0091e77faf454502
Bruno Fenzl <brunofenzl@gmail.com>
2020-02-11 17:06:52 +0100

    refactoring add/select components

--- src/app/content/store/effects/page.effects.ts
+++ src/app/content/store/effects/page.effects.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Effect, ofType, Actions } from '@ngrx/effects';
-import { Observable } from 'rxjs';
-import { mergeMap } from 'rxjs/operators';
-import { ContentActionTypes, ChooseComponentAction, PageActionTypes } from '../actions';
-import { NetworkService } from '../../../services/network.service';
-import { Action } from '@ngrx/store';
+import { Actions } from '@ngrx/effects';
 import { ModalService } from 'src/app/modal/modal.service';
 
 @Injectable()
--- src/app/services/page.service.ts
+++ src/app/services/page.service.ts
@@ -2,13 +2,40 @@ import { DynamicPageSchema, DynamicComponentSchema } from 'src/dynamic-renderer/
 import { LoadPageAction, AddPageAction, UpdatePageAction, DeletePageAction, ContentState, AddComponentAction } from '../content/store';
 import { Store } from '@ngrx/store';
 import { Injectable } from '@angular/core';
+import { RowDefaults } from '../content/dynamic-components/row';
+import { ColumnDefaults } from '../content/dynamic-components/column';
+import { HeadlineDefaults } from '../content/dynamic-components/headline';
+import { ParagraphDefaults } from '../content/dynamic-components/paragraph';
+import { TextareaDefaults } from '../content/dynamic-components/textarea';
+import { TextInputDefaults } from '../content/dynamic-components/text-input';
+import { NumberInputDefaults } from '../content/dynamic-components/number-input';
+import { SelectDefaults } from '../content/dynamic-components/select';
+import { SwitchDefaults } from '../content/dynamic-components/switch';
 
 @Injectable()
 export class PageService {
 
+  private comps: { [id: string]: any };
+
+  public get components(): { [id: string]: any } {
+    return this.comps;
+  }
+
   constructor(
     private store: Store<ContentState>
-  ) { }
+  ) {
+    this.comps = {
+      Row: RowDefaults,
+      Column: ColumnDefaults,
+      Headline: HeadlineDefaults,
+      Paragraph: ParagraphDefaults,
+      Textarea: TextareaDefaults,
+      'Text Input': TextInputDefaults,
+      'Number Input': NumberInputDefaults,
+      Select: SelectDefaults,
+      Switch: SwitchDefaults,
+    };
+  }
 
   loadPage(pageId: string): void {
     this.store.dispatch(new LoadPageAction(pageId));
@@ -37,5 +64,10 @@ export class PageService {
       component: comp,
       parentId,
     }));
+  }
+
+  selectComponent(key): void {
+    const newComp = new this.components[key]();
+    this.addComponent(newComp);
   }
 }
