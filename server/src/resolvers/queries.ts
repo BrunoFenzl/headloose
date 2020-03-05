29f3e2122ca026ca8bede15b7de6a727925c9dd1
Bruno Fenzl <brunofenzl@gmail.com>
2020-03-06 00:04:57 +0100

    wip queries

--- server/src/resolvers/queries.ts
+++ server/src/resolvers/queries.ts
@@ -1,14 +1,50 @@
-
+import { ComponentModel } from '../db';
 
 export const Query = {
-  tree: async (parent, { id }, ctx, info) => { },
-  component: async () => { },
-  components: async (parent, { ids }, ctx, info) => {
-    console.log(parent, ids, ctx, info);
+  tree: async (parent, { id }, ctx, info) => {
+    const docs = [];
+    async function getDoc(_id) {
+      const res = await ComponentModel.findById(_id);
+      return res;
+    }
+
+    async function processData(_id) {
+      const doc = await getDoc(_id);
+
+      if (doc.children) {
+        for (let i = 0; i < doc.children.length; i++) {
+          const newId = doc.children[i];
+          docs.push(await processData(newId));
+        }
+      }
+
+      return doc;
+    }
+
+    docs.push(await processData(id));
+
     return {
       code: 200,
       message: 'all good',
-      data: [],
+      data: docs,
+    }
+  },
+  component: async (parent, { id }, ctx, info) => {
+    const doc = await ComponentModel.findById(id);
+
+    return {
+      code: 200,
+      message: 'all good',
+      data: [doc],
+    }
+  },
+  components: async (parent, { ids }, ctx, info) => {
+    const docs = await ComponentModel.find().where('_id').in(ids).exec();
+
+    return {
+      code: 200,
+      message: 'all good',
+      data: docs,
     }
   },
 }
